efb9d7f009bbb527c52cc590a26b68bae291ecf1
root <root@debug010000002015.local.alipay.net>
2017-07-16 17:29:18 +0800

    record all but Crts& venv

---
+++ Main.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+import urllib
+import urllib2
+import json
+#from DeEnc.WXCrypt import WXBizDataCrypt as WXCrypt
+from flask import Flask,request,jsonify
+from ext import db
+from users import User
+app = Flask(__name__)
+app.config.from_object('config')
+db.init_app(app)
+audioInfo ={
+		'name':'Testing title from server',
+		'author':'Sammuel Zebradoe',
+		'src':	'http://szdatabucket1-1253981734.cossh.myqcloud.com/lxAudio_40_Days/lsn_1.mp3'#'http://dx.sc.chinaz.com/Files/DownLoad/sound1/201707/8934.mp3'
+	}
+
+@app.route('/tag/')
+def index():
+	return "SZ loves Ana"
+@app.route('/brothers/')
+def brothers():
+	return "小熊，兔纸，你们要加油啊，哈哈哈哈哈哈哈哈"
+@app.route('/room506/')
+def roommate():
+	return "山舟，来，周嘉涛，秦王元；\n猴季，Mr.Sulu，大磊，米老"
+@app.route('/server/audioInfo/')
+def wx_server():
+	return jsonify({'audioInfo':audioInfo})
+@app.route('/onLogin/',methods=['GET','POST'])
+def wx_Login():
+	appId = 'wxde4ed04d17675e14'
+	if request.method=='POST':
+		req_data = json.loads(request.get_data())
+		app.logger.debug('res')
+		app.logger.debug(json.dumps(req_data))
+		#return req_data[u'code']
+		#encData = req_data.encryptedData
+		url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wxde\
+4ed04d17675e14&secret=c12283859e80a742ef233b73eafaff33&\
+js_code='+req_data[u'code']+'&grant_type=authorization_code'
+		app.logger.debug(url)
+		f = urllib2.urlopen(url)
+		res = json.loads(f.read())
+		f.close()
+		app.logger.debug(res)		#iv = req_data.iv
+		#session_key and openid should be saved, while session_key shouldn't be transed by web
+		try:
+			return jsonify({'openid':res[u'openid']})
+		except:
+			return res[u'errmsg']
+#		if iv!='skipped':
+#			signature = req_data.signature
+#			sessionKey = req_ss.sessionKey
+#			pc = WXCrypt(appId,sessionKey)
+#			dncData = pc.decryptData(encData,iv)
+	else:
+		return '<h1> you are not allwed to get anything unless post<h1>'
+app.run('0.0.0.0',debug=True,port=443,ssl_context=("Crts/214194239870590.pem",
+"Crts/214194239870590.key"))
+
---
+++ consts.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+HOSTNAME = 'rm-uf673d3f5x9sb4sq7.mysql.rds.aliyuncs.com:3306'
+DATABASE = 'app_sch'
+USERNAME = 'szkd_app_acc'
+PASSWORD = 'szkd_app_access_586'
+DB_URI = 'mysql://{}:{}@{}/{}'.format(USERNAME,PASSWORD,HOSTNAME,DATABASE)
