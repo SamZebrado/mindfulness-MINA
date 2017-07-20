14c6b591973b8c766bc8c1dbc07b57a630c51fd1
SamZebrado <1005015199@qq.com>
2017-07-20 13:24:44 +0000

    debug using spyder ok wtih the help of ZOEWORKER2 and my dear Ana

--- testing_py.py
+++ testing_py.py
@@ -1,6 +1,6 @@
 from models import *
-i1,r = Identity.update_id_unique_record(openid = 'xyszkd',uploaded_data="I'm feedback<ptn_\
-I'm d r<ptn_>")
-i2 = i1.merge_ptn_data(short_dt="I'm B feedback<ptn_>I'm new B<ptn_>")
+i1,r = Identity.update_id_unique_record(openid = 'xyszkd',uploaded_data="abc<ptn_>bcd<ptn_>email<ptn_>Old feedback<ptn_>\
+old Rating")
+i2 = i1.merge_ptn_data(short_dt="New feedback<ptn_>~~~new B")
 print i2.fdbck
 
